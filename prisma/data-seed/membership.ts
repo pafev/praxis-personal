3f3cca55255f478d2109d6803c2851c552beab34
pafev <paulofvilarimda@gmail.com>
2024-08-05 21:43:44 -0300

    fix images members

--- prisma/data-seed/membership.ts
+++ prisma/data-seed/membership.ts
@@ -3,13 +3,13 @@ export const memberships = [
     name: "Geovana",
     role: "Projetos",
     profilePicture:
-      "https://res.cloudinary.com/dlwxmf47w/image/upload/v1722888377/diretoria-comercial-nobg_j4xfvi.png",
+      "https://res.cloudinary.com/dlwxmf47w/image/upload/v1722888377/diretoria-projetos-nobg_x6yqbx.png",
   },
   {
     name: "Catharina",
     role: "Comercial",
     profilePicture:
-      "https://res.cloudinary.com/dlwxmf47w/image/upload/v1722888377/diretoria-projetos-nobg_x6yqbx.png",
+      "https://res.cloudinary.com/dlwxmf47w/image/upload/v1722888377/diretoria-comercial-nobg_j4xfvi.png",
   },
   {
     name: "Júlia",
